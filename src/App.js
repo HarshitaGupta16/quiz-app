ee7902b3e4e69dc68ca7a7da966443049cd248aa
Harshita Gupta <sign.of.happiness@gmail.com>
2022-09-12 01:53:15 +0530

    quiz application initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Home from './Pages/Home/Home';
+import Quiz from './Pages/Quiz/Quiz';
+import Result from './Pages/Result/Result';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [name, setName] = useState("")
+  const [questions, setQuestions] = useState();
+  const [score, setScore] = useState(0);
+
+  // by default category and difficulty going to be empty
+  const fetchQuestions = async( category='', difficulty='') => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${category && `&category=${category}`}${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    )
+    
+    setQuestions(data.results)
+    console.log(data.results)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app" >
+        <Header />
+        <Routes>
+          <Route path='/' exact element={<Home name={name} setName={setName} fetchQuestions={fetchQuestions} />} />
+          <Route path='/quiz' exact element={
+            <Quiz 
+              name={name} 
+              questions={questions}
+              score={score}
+              setScore={setScore}
+              setQuestions={setQuestions}
+            />
+            } />
+          <Route path='/result' exact element={<Result name={name} score={score} />} />
+        </Routes>
+      </div>
+        <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      Made by {""}
+      <a href='https://harshitagupta.netlify.app/' className='link'>
+        Harshita Gupta
+      </a>
+    </div>
+  )
+}
+
+export default Footer
